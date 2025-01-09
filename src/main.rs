cc8269794310c4603fd2b377e59c1970d4e7ee3f
cxllmerichie <contact@cxllmerichie.com>
2025-01-09 16:38:21 +0200

    prev resolved, `edge_nal_std` is the last visible issue now

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,10 @@ use smoltcp::{
 
 // ANCHOR: server_deps
 use core::fmt::{Debug, Display};
-//
 use edge_http::io::server::{Connection, DefaultServer, Handler};
 use edge_http::Method;
 use edge_nal::TcpBind;
-//
-// use embedded_io_async::{Read, Write};
-//
+use embedded_io_async::{Read, Write};
 use log::info;
 // ANCHOR: debug_deps
 use anyhow;
@@ -191,50 +188,50 @@ fn main() -> ! {
     }
 }
 
-// pub async fn run(server: &mut DefaultServer) -> Result<(), anyhow::Error> {
-//     let addr = "0.0.0.0:8881";
-//
-//     info!("Running HTTP server on {addr}");
-//
-//     let acceptor = edge_nal_std::Stack::new()
-//         .bind(addr.parse().unwrap())
-//         .await?;
-//
-//     server.run(None, acceptor, HttpHandler).await?;
-//
-//     Ok(())
-// }
-//
-// struct HttpHandler;
-//
-// impl Handler for HttpHandler {
-//     type Error<E>
-//     = edge_http::io::Error<E>
-//     where
-//         E: Debug;
-//
-//     async fn handle<T, const N: usize>(
-//         &self,
-//         _task_id: impl Display + Copy,
-//         conn: &mut Connection<'_, T, N>,
-//     ) -> Result<(), Self::Error<T::Error>>
-//     where
-//         T: Read + Write,
-//     {
-//         let headers = conn.headers()?;
-//
-//         if headers.method != Method::Get {
-//             conn.initiate_response(405, Some("Method Not Allowed"), &[])
-//                 .await?;
-//         } else if headers.path != "/" {
-//             conn.initiate_response(404, Some("Not Found"), &[]).await?;
-//         } else {
-//             conn.initiate_response(200, Some("OK"), &[("Content-Type", "text/plain")])
-//                 .await?;
-//
-//             conn.write_all(b"Hello world!").await?;
-//         }
-//
-//         Ok(())
-//     }
-// }
+pub async fn run(server: &mut DefaultServer) -> Result<(), anyhow::Error> {
+    let addr = "0.0.0.0:8881";
+
+    info!("Running HTTP server on {addr}");
+
+    let acceptor = edge_nal_std::Stack::new()
+        .bind(addr.parse().unwrap())
+        .await?;
+
+    server.run(None, acceptor, HttpHandler).await?;
+
+    Ok(())
+}
+
+struct HttpHandler;
+
+impl Handler for HttpHandler {
+    type Error<E>
+    = edge_http::io::Error<E>
+    where
+        E: Debug;
+
+    async fn handle<T, const N: usize>(
+        &self,
+        _task_id: impl Display + Copy,
+        conn: &mut Connection<'_, T, N>,
+    ) -> Result<(), Self::Error<T::Error>>
+    where
+        T: Read + Write,
+    {
+        let headers = conn.headers()?;
+
+        if headers.method != Method::Get {
+            conn.initiate_response(405, Some("Method Not Allowed"), &[])
+                .await?;
+        } else if headers.path != "/" {
+            conn.initiate_response(404, Some("Not Found"), &[]).await?;
+        } else {
+            conn.initiate_response(200, Some("OK"), &[("Content-Type", "text/plain")])
+                .await?;
+
+            conn.write_all(b"Hello world!").await?;
+        }
+
+        Ok(())
+    }
+}
