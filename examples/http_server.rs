caa3a4d719562d5c05dc02ee032ab419c7cf71a3
cxllmerichie <contact@cxllmerichie.com>
2025-01-12 21:03:45 +0200

    flashed one; todo: move from lib structure to project with esp-idf-svc as dependency

--- examples/http_server.rs
+++ examples/http_server.rs
@@ -1,15 +1,10 @@
-//! HTTP Server with JSON POST handler
-//!
-//! Go to 192.168.71.1 to test
-
 use core::convert::TryInto;
-
 use embedded_svc::{
     http::{Headers, Method},
     io::{Read, Write},
-    wifi::{self, AccessPointConfiguration, AuthMethod},
+    wifi::{self, AuthMethod, ClientConfiguration, Configuration},
 };
-
+use esp_idf_hal::gpio::PinDriver;
 use esp_idf_svc::hal::prelude::Peripherals;
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
@@ -17,134 +12,127 @@ use esp_idf_svc::{
     nvs::EspDefaultNvsPartition,
     wifi::{BlockingWifi, EspWifi},
 };
-
-use log::*;
+use std::sync::{Arc, Mutex};
 
 use serde::Deserialize;
 
 const SSID: &str = env!("WIFI_SSID");
 const PASSWORD: &str = env!("WIFI_PASS");
-static INDEX_HTML: &str = include_str!("http_server_page.html");
+static FAVICON_ICO: &[u8] = include_bytes!("favicon.ico");
 
-// Max payload length
-const MAX_LEN: usize = 128;
-
-// Need lots of stack to parse JSON
-const STACK_SIZE: usize = 10240;
-
-// Wi-Fi channel, between 1 and 11
-const CHANNEL: u8 = 11;
-
-#[derive(Deserialize)]
-struct FormData<'a> {
-    first_name: &'a str,
-    age: u32,
-    birthplace: &'a str,
-}
-
-fn main() -> anyhow::Result<()> {
+fn main() -> ! {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    // Setup Wifi
-
-    let peripherals = Peripherals::take()?;
-    let sys_loop = EspSystemEventLoop::take()?;
-    let nvs = EspDefaultNvsPartition::take()?;
+    let peripherals = Peripherals::take().unwrap();
+    let sys_loop = EspSystemEventLoop::take().unwrap();
+    let nvs = EspDefaultNvsPartition::take().unwrap();
 
     let mut wifi = BlockingWifi::wrap(
-        EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
+        EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs)).unwrap(),
         sys_loop,
-    )?;
+    )
+    .unwrap();
 
-    connect_wifi(&mut wifi)?;
+    connect(&mut wifi).unwrap();
 
-    let mut server = create_server()?;
+    let pin = Arc::new(Mutex::new(
+        PinDriver::output(peripherals.pins.gpio4).unwrap(),
+    ));
 
-    server.fn_handler("/", Method::Get, |req| {
-        req.into_ok_response()?
-            .write_all(INDEX_HTML.as_bytes())
-            .map(|_| ())
-    })?;
+    let mut server = create().unwrap();
 
-    server.fn_handler::<anyhow::Error, _>("/post", Method::Post, |mut req| {
-        let len = req.content_len().unwrap_or(0) as usize;
+    server
+        .fn_handler("/favicon.ico", Method::Get, |req| {
+            req
+                .into_ok_response()
+                .unwrap()
+                .write_all(FAVICON_ICO)
+        })
+        .unwrap();
 
-        if len > MAX_LEN {
-            req.into_status_response(413)?
-                .write_all("Request too big".as_bytes())?;
-            return Ok(());
-        }
+    let pinref = Arc::clone(&pin);
+    server
+        .fn_handler("/", Method::Get, move |req| {
+            let mut gpio = pinref.lock().unwrap();
 
-        let mut buf = vec![0; len];
-        req.read_exact(&mut buf)?;
-        let mut resp = req.into_ok_response()?;
+            gpio
+                .toggle()
+                .unwrap();
 
-        if let Ok(form) = serde_json::from_slice::<FormData>(&buf) {
-            write!(
-                resp,
-                "Hello, {}-year-old {} from {}!",
-                form.age, form.first_name, form.birthplace
-            )?;
-        } else {
-            resp.write_all("JSON error".as_bytes())?;
-        }
+            let response = if gpio.is_set_low() { "OFF" } else { "ON" };
 
-        Ok(())
-    })?;
+            req
+                .into_ok_response()
+                .unwrap()
+                .write_all(response.as_bytes())
+        })
+        .unwrap();
 
-    // Keep wifi and the server running beyond when main() returns (forever)
-    // Do not call this if you ever want to stop or access them later.
-    // Otherwise you can either add an infinite loop so the main task
-    // never returns, or you can move them to another thread.
-    // https://doc.rust-lang.org/stable/core/mem/fn.forget.html
-    core::mem::forget(wifi);
-    core::mem::forget(server);
+    let pinref = Arc::clone(&pin);
+    server
+        .fn_handler("/off", Method::Get, move |req| {
+            pinref
+                .lock()
+                .unwrap()
+                .set_low()
+                .unwrap();
 
-    // Main task no longer needed, free up some memory
-    Ok(())
+            req
+                .into_ok_response()
+                .unwrap()
+                .write_all("OFF".as_bytes())
+        })
+        .unwrap();
+
+    let pinref = Arc::clone(&pin);
+    server
+        .fn_handler("/on", Method::Get, move |req| {
+            pinref
+                .lock()
+                .unwrap()
+                .set_high()
+                .unwrap();
+
+            req
+                .into_ok_response()
+                .unwrap()
+                .write_all("ON".as_bytes())
+        })
+        .unwrap();
+
+    loop {
+        std::thread::sleep(core::time::Duration::from_secs(1));
+    }
 }
 
-fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()> {
-    // If instead of creating a new network you want to serve the page
-    // on your local network, you can replace this configuration with
-    // the client configuration from the http_client example.
-    let wifi_configuration = wifi::Configuration::AccessPoint(AccessPointConfiguration {
+fn connect(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()> {
+    let config: Configuration = Configuration::Client(ClientConfiguration {
         ssid: SSID.try_into().unwrap(),
-        ssid_hidden: true,
+        bssid: None,
         auth_method: AuthMethod::WPA2Personal,
         password: PASSWORD.try_into().unwrap(),
-        channel: CHANNEL,
+        channel: None,
         ..Default::default()
     });
 
-    wifi.set_configuration(&wifi_configuration)?;
+    wifi.set_configuration(&config)?;
 
     wifi.start()?;
-    info!("Wifi started");
+    log::info!("Wifi started");
 
-    // If using a client configuration you need
-    // to connect to the network with:
-    //
-    //  ```
-    //  wifi.connect()?;
-    //  info!("Wifi connected");
-    // ```
+    wifi.connect()?;
+    log::info!("Wifi connected");
 
     wifi.wait_netif_up()?;
-    info!("Wifi netif up");
-
-    info!(
-        "Created Wi-Fi with WIFI_SSID `{}` and WIFI_PASS `{}`",
-        SSID, PASSWORD
-    );
+    log::info!("Wifi netif up");
 
     Ok(())
 }
 
-fn create_server() -> anyhow::Result<EspHttpServer<'static>> {
+fn create() -> anyhow::Result<EspHttpServer<'static>> {
     let server_configuration = esp_idf_svc::http::server::Configuration {
-        stack_size: STACK_SIZE,
+        stack_size: 10240usize,
         ..Default::default()
     };
 
